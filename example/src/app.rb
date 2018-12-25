e02aa6b3607a7b2484252aa73b236330e87dcbb2
Kevin Hughes <kevinhughes27@gmail.com>
2018-12-25 20:17:44 +0700

    add shop_origin method for esdk

--- example/src/app.rb
+++ example/src/app.rb
@@ -12,7 +12,7 @@ class SinatraApp < Sinatra::Base
   # from Shopify and displays them inside your app
   get '/' do
     shopify_session do |shop_name|
-      @shop_name = shop_name
+      @shop = ShopifyAPI::Shop.current
       @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
       erb :home
     end
