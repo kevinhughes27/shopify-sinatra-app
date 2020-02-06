e17cd86e55a69ab40a41da559ad6c6f457bdb145
Kevin Hughes <kevinhughes27@gmail.com>
2020-02-06 10:28:06 -0500

    add api_version to settings

--- example/test/app_test.rb
+++ example/test/app_test.rb
@@ -23,8 +23,9 @@ class AppTest < Minitest::Test
 
   def test_root_with_session
     set_session
-    fake 'https://testshop.myshopify.com/admin/api/2019-04/shop.json', body: {myshopify_domain: @shop_name}.to_json
-    fake 'https://testshop.myshopify.com/admin/api/2019-04/products.json?limit=10', body: '{}'
+    api_url = "https://testshop.myshopify.com/admin/api/#{app.settings.api_version}"
+    fake "#{api_url}/shop.json", body: {myshopify_domain: @shop_name}.to_json
+    fake "#{api_url}/products.json?limit=10", body: '{}'
     get '/'
     assert last_response.ok?
   end
--- lib/sinatra/shopify-sinatra-app.rb
+++ lib/sinatra/shopify-sinatra-app.rb
@@ -83,7 +83,7 @@ module Sinatra
       end
 
       def activate_shopify_api(shop_name, token)
-        api_session = ShopifyAPI::Session.new(domain: shop_name, token: token, api_version: '2019-04')
+        api_session = ShopifyAPI::Session.new(domain: shop_name, token: token, api_version: settings.api_version)
         ShopifyAPI::Base.activate_session(api_session)
       end
 
@@ -167,6 +167,7 @@ module Sinatra
       app.enable :sessions
       app.enable :inline_templates
 
+      app.set :api_version, '2019-07'
       app.set :scope, 'read_products, read_orders'
 
       app.set :api_key, ENV['SHOPIFY_API_KEY']
