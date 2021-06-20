aa941525d4d3bbfe2ea415d38ed40b6130e0a025
Kevin Hughes <kevinhughes27@gmail.com>
2021-06-20 12:37:47 -0400

    update ci script. remove travis

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+set -e
+
 cd example
 
 bundle install
 bundle exec rake db:migrate
 bundle exec rake test:prepare
 bundle exec rake test
-EXIT_CODE=$?
-
-cd ../../..
-
-exit $EXIT_CODE
