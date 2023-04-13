838f752810ecf89a85868bd9db66ca06cfad8961
Kevin Hughes <kevinhughes27@gmail.com>
2023-04-13 10:00:58 -0400

    update deps again

--- example/db/schema.rb
+++ example/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014_04_14_042317) do
-
+ActiveRecord::Schema[7.0].define(version: 2014_04_14_042317) do
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.string "token_encrypted"
